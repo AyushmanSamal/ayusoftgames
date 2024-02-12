c2b96cb5862c1da749603f13b0068ed2913e471a
AyushmanSamal <samalayushman06@gmail.com>
2024-02-12 22:39:27 +0530

    nigga

--- static/scrpt.js
+++ static/scrpt.js
@@ -16,3 +16,27 @@ document.getElementById('menuButton').addEventListener('click', function () {
   const menu = document.getElementById('menu');
   menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
 });
+
+win_x = (window.innerWidth * 0.95);
+win_y = (window.innerHeight * 0.70);
+
+document.addEventListener('DOMContentLoaded', function(){
+  var rcb = document.getElementById('rcb');
+
+  if (rcb){
+    rcb.style.width = win_x + 'px';
+    rcb.style.height = rcb.scrollHeight + 10 + 'px';
+    rcb.style.backgroundColor = '#3c3b3d';
+    rcb.style.borderRadius = '20px';
+    rcb.style.boxShadow = '5px 5px 10px rgb(0, 0, 20)';
+    rcb.style.marginLeft = 'auto';
+    rcb.style.marginRight = 'auto';
+    console.log("Yeah got it");
+  }else{
+    console.error("Fuck u!")
+  }
+  
+
+  console.log(win_y);
+});
+
